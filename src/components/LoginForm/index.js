03389b5f6c450e95c8e787f778512c5b7c6f80d8
ramakrishna <ramakrishna516@gmail.com>
2023-06-30 14:46:05 +0000

    commit message

---
+++ src/components/LoginForm/index.js
@@ -0,0 +1,117 @@
+// Write your JS code here
+import {Component} from 'react'
+
+import './index.css'
+
+class LoginForm extends Component {
+  state = {
+    username: '',
+    password: '',
+    isLoad: false,
+    errorMsg: '',
+  }
+
+  onChangeUsername = event => {
+    this.setState({username: event.target.value})
+  }
+
+  onChangePassword = event => {
+    this.setState({password: event.target.value})
+  }
+
+  onSubmit = async event => {
+    event.preventDefault()
+    const {username, password} = this.state
+    const userDetails = {username, password}
+    const url = 'https://apis.ccbp.in/login'
+
+    const option = {
+      method: 'POST',
+      body: JSON.stringify(userDetails),
+    }
+    const response = await fetch(url, option)
+    const data = await response.json()
+
+    if (response.ok === true) {
+      this.submitSucess()
+    } else {
+      this.setState({isLoad: true, errorMsg: data.error_msg})
+    }
+  }
+
+  submitSucess = () => {
+    const {history} = this.props
+    history.replace('/')
+  }
+
+  renderPasswordField = () => {
+    const {password} = this.state
+    return (
+      <>
+        <label className="input-label" htmlFor="password">
+          PASSWORD
+        </label>
+        <input
+          placeholder="Password"
+          type="password"
+          id="password"
+          className="password-input-filed"
+          value={password}
+          onChange={this.onChangePassword}
+        />
+      </>
+    )
+  }
+
+  renderUsernameField = () => {
+    const {username} = this.state
+    return (
+      <>
+        <label className="input-label" htmlFor="username">
+          USERNAME
+        </label>
+        <input
+          placeholder="Username"
+          type="text"
+          id="username"
+          className="username-input-filed"
+          value={username}
+          onChange={this.onChangeUsername}
+        />
+      </>
+    )
+  }
+
+  render() {
+    const {isLoad, errorMsg} = this.state
+    return (
+      <div className="login-form-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+          className="login-website-logo-mobile-image"
+          alt="website logo"
+        />
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-login-img.png"
+          className="login-image"
+          alt="website login"
+        />
+        <form onClick={this.onSubmit} className="form-container">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+            className="login-website-logo-desktop-image"
+            alt="website logo"
+          />
+          <div className="input-container">{this.renderUsernameField()}</div>
+          <div className="input-container">{this.renderPasswordField()}</div>
+          <button type="submit" className="login-button">
+            Login
+          </button>
+          {isLoad && <p>*{errorMsg}</p>}
+        </form>
+      </div>
+    )
+  }
+}
+
+export default LoginForm
